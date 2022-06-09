dd619e72493873575882f77fb90532b078b99084
Ryli Davis <rylidavis94@gmail.com>
2022-06-09 03:35:53 -0700

    Append Elements to DOM for Function showPage

--- js/script.js
+++ js/script.js
@@ -23,20 +23,53 @@ function showPage(list, page) {
    const endIndex = (page * 9);
    let studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
-   for(let i = 0; i < list.length; i++) {
-      if([i] >= startIndex && [i] < endIndex) {
+   for (let i = 0; i < list.length; i++) {
+      if ([i] >= startIndex && [i] < endIndex) {
          let li = document.createElement('li');
          li.className = 'student-item cf';
+         studentList.append(li);
+
+         let studentlistDiv = document.createElement('div');
+         studentlistDiv.className = 'student-details';
+         li.append(studentlistDiv);
+
+         let img = document.createElement('img');
+         img.className = 'avatar';
+         img.src = data[i].picture.medium;
+         img.alt = 'Profile Picture';
+         studentlistDiv.append(img);
+
+         let h3 = document.createElement('h3');
+         h3.innerHTML = data[i].name.first + ' ' + data[i].name.last;
+         studentlistDiv.append(h3);
+
+         let spanEmail = document.createElement('span');
+         spanEmail.className = 'email';
+         spanEmail.innerHTML = data[i].email;
+         studentlistDiv.append(spanEmail);
+
+         let joineddetailsDiv = document.createElement('div');
+         joineddetailsDiv.className = 'joined-details';
+         li.append(joineddetailsDiv);
+
+         let spanDate = document.createElement('span');
+         spanDate.className = 'date';
+         spanDate.innerHTML = 'Joined' + ' ' + data[i].registered.date;
+         joineddetailsDiv.append(spanDate);
+
+
+
       }
    }
-
 }
+
+
+
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
 
 
 // Call functions
